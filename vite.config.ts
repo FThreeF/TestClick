37a4434f544fa18decf83fc2822386b52b13c61c
FThreeF <fthreef.535@gmail.com>
2026-06-24 00:30:40 +0300

    vf

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/TestClick/",
   resolve: {
     alias: {
       "@modules": path.resolve(__dirname, "./src/modules"),
